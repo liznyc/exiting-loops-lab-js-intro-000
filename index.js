9bb3a1334d2a8560377305418aed47dd43a921da
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-04 21:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
       if (findFn(array[i])) {
-        return array[i]
-      }else {
-        return "null";
+        return array[i];
+      } else {
+        return null;
       }
     }
 
